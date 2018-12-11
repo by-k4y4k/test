6054aff342edb137707dac233bfe8267638a1bf7
kayak <41324271+by-k4y4k@users.noreply.github.com>
2018-12-11 14:35:32 +1000

    route parameters example

--- Section 25 - Server Side Frameworks/1. Baby's First Express App/app.js
+++ Section 25 - Server Side Frameworks/1. Baby's First Express App/app.js
@@ -2,25 +2,37 @@ var express = require("express");
 var app = express();
 
 // when GET "/"
+// "/" => "hi there"
 app.get("/", function(req, res) {
   res.send("whats cookin");
 });
 
+// "/bye" => "cya!"
 app.get("/bye", function(req, res) {
   res.send("cya!");
 });
 
+// "/dog" => "meow"
 app.get("/dog", function(req, res) {
   res.send(">:3 meow");
+});
+
+app.get("/r/:subredditName", function(req, res) {
+  let subreddit = req.params.subredditName;
+  // log parameters to terminal
+  console.log(req.params);
+  res.send("welcome to the '" + subreddit + "' subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+  // log parameters to terminal
+  console.log(req.params);
+  res.send("this is the comments page");
 });
 
 app.get("*", function(req, res) {
   res.send("ur a star");
 });
-
-// "/" => "hi there"
-// "/bye" => "cya!"
-// "/dog" => "meow"
 
 // Tell Express to listen for requests
 app.listen(2121, "localhost", function() {
